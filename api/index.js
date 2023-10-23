6d887e778f91c12393a81b1908dbf5803b88f850
Suman Kumar <suman.kumar.it.2020@tint.edu.in>
2023-10-23 22:26:03 +0530

    minor change in the payload size

--- api/index.js
+++ api/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(express.json({ limit: "50mb" }));
 app.use(
-  express.urlencoded({ parameterLimit: 100000, limit: "50mb", extended: false })
+  express.urlencoded({ parameterLimit: 100000, limit: "200mb", extended: false })
 );
 
 app.use(cors());
